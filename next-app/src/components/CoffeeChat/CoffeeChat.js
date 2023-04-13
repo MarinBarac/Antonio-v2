d6a7032246bb2b1fdc1dbe299cdaad194a47a1e1
unknown <marinbarac21@gmail.com>
2023-04-13 13:13:34 +0200

    Minor fix

--- next-app/src/components/CoffeeChat/CoffeeChat.js
+++ next-app/src/components/CoffeeChat/CoffeeChat.js
@@ -7,9 +7,14 @@ import clsx from "clsx";
 import { useContext } from "react";
 
 import styles from "./CoffeeChat.module.scss";
+import Link from "next/link";
 
 const CoffeeChat = () => {
   const { isDark } = useContext(ThemeContext);
+
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth();
 
   return (
     <section className="section">
@@ -19,8 +24,19 @@ const CoffeeChat = () => {
           description="No strings attached. Book a 30 minute video call via Calendly or send an email."
         />
         <div className={styles.buttons}>
-            <Button href="/" variant="primary">Book a call</Button>
-            <Button href="/contact" variant="secondary">E-mail</Button>
+          <Link
+            href={`https://calendly.com/antoniovidakovic/30min?month=${year}-${
+              +month < 10 ? "0" + month : month
+            }`}
+            target="_blank"
+          >
+            <Button href="/" variant="primary">
+              Book a call
+            </Button>
+          </Link>
+          <Link href="/contact">
+            <Button variant="secondary">E-mail</Button>
+          </Link>
         </div>
       </div>
     </section>
