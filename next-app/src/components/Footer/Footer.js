64c76cbd9e9c497a85dde6a3ff2288f02bedc865
Marin Barac <marinbarac21@gmail.com>
2023-07-24 18:54:14 +0200

    Changed footer text

--- next-app/src/components/Footer/Footer.js
+++ next-app/src/components/Footer/Footer.js
@@ -13,23 +13,31 @@ const Footer = () => {
         <ul className={styles.siteLinks}>
           {SITE_LINKS.map((link) => (
             <li key={"Footer link to " + link.text}>
-              <CustomLink href={link.href} type="footer-link">{link.text}</CustomLink>
+              <CustomLink href={link.href} type="footer-link">
+                {link.text}
+              </CustomLink>
             </li>
           ))}
         </ul>
-        <div className={styles.dividerLine}/>
+        <div className={styles.dividerLine} />
         <ul className={styles.socialMedia}>
           {SOCIAL_MEDIA_LINKS.map((link) => (
             <li key={link.key}>
-              <Link href={link.href} target="_blank">{link.icon}</Link>
+              <Link href={link.href} target="_blank">
+                {link.icon}
+              </Link>
             </li>
           ))}
         </ul>
       </div>
       <div className={styles.dividerLine} />
-      <p className={styles.legalText}>
-        &copy; 2023 VIDAKOVIC All rights reserved.
-      </p>
+      <div className={styles.legalText}>
+        <p>
+          VIDAKOVIC, obrt za računalne usluge, vl. Antonio Vidaković, Kaštel
+          Štafilić,Koludrovac 24
+        </p>
+        <p>&copy; 2023 VIDAKOVIC All rights reserved.</p>
+      </div>
     </footer>
   );
 };
