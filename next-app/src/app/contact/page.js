6b156f0c4befe4a4e9ba80edff3803c01a759b21
Marin Barac <marinbarac21@gmail.com>
2023-08-02 18:28:51 +0200

    Fixed vercel error

--- next-app/src/app/contact/page.js
+++ next-app/src/app/contact/page.js
@@ -1,9 +1,9 @@
+import config from "@config/site";
 import { openGraph } from "shared/sharedMetadata";
 const { default: ContactPage } = require("@modules/contact");
 
-require("dotenv").config();
 export const metadata = {
-  metadataBase: new URL(`${process.env.SITE_URL}contact`),
+  metadataBase: new URL(`${config.siteUrl}contact`),
   title: "Contact Antonio Vidakovic - Freelance UX/UI Designer for Hire",
   description:
     "Are you looking for a freelance UX/UI designer to help you improve your product and solve user problems? Let's collaborate! Contact me today to discuss your project and learn how I can help you achieve your goals.",
